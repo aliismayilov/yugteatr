2f2e92b04137dafa22f8a7063e1cfcb66bbccdcb
Ali Ismayilov <ali@ismailov.info>
2013-04-27 21:05:36 +0500

    add Language and Page models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.SlugField(max_length=10, unique=True)
+    order = models.IntegerField(null=True, blank=True) # optional order field
+
+    class Meta:
+        ordering = ['order']
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/%s' % self.name
+
+class Page(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    body = models.TextField()
+    language = models.ForeignKey('Language')
+
+    url = models.CharField(max_length=200, blank=True, null=True)
+    order = models.IntegerField(null=True, blank=True) # optional order field
+
+    # parent page
+    parent = models.ForeignKey('self', null=True, blank=True)
+
+    class Meta:
+        ordering = ['order']
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        if self.url:
+            return self.url
+        else:
+            return '/%s/%s.html' % (self.language, self.slug)
