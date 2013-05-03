6505d4fe7826ec3b36be88a84cb0ddeb4d767de7
Ali Ismayilov <ali@ismailov.info>
2013-05-04 01:25:02 +0500

    add index view and template

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from django.views.generic.base import TemplateView
 
 
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="web/base.html")),
+urlpatterns = patterns('web.views',
+    url(r'^$', 'index', name='index'),
 )
--- web/views.py
+++ web/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from web.models import Page, Language
+
+def index(request, language_slug=None):
+	if language_slug:
+		current_language = get_object_or_404(Language, name=language_slug)
+	else:
+		current_language = Language.objects.all()[0]
+
+
+	context = {
+		'parent_pages': Page.objects.filter(parent=None),
+		'languages': Language.objects.all(),
+		'current_language': current_language,
+	}
+	return render(request, 'web/index.html', context)
