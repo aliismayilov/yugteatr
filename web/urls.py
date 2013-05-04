dc07c1f7648ad8eabca4fe0a270eaebdba5c4e12
Ali Ismayilov <ali@ismailov.info>
2013-05-04 15:36:10 +0500

    change order of urls

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('web.views',
-    url(r'^$', 'index', name='index'),
+	url(r'^(?P<language_slug>[a-z]{2,3})/(?P<page_slug>(\w|-)+)\.html$', 'show_page', name='show_page'),
     url(r'^(?P<language_slug>[a-z]{2,3})$', 'index', name='index'),
-    url(r'^(?P<language_slug>[a-z]{2,3})/(?P<page_slug>(\w|-)+)\.html$', 'show_page', name='show_page'),
+    url(r'^$', 'index', name='index'),
 )
